533631310e596eabb77f81d682c1fa0625278a8f
xAnonym101 <xvgonst@gmail.com>
2024-10-13 17:23:27 +0700

    Second Revision

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -5,117 +5,52 @@ import streamlit as st
 from babel.numbers import format_currency
 sns.set(style='dark')
 
-def create_total_df(df):
-    total_df = df.groupby(by="dteday").agg({"cnt": "sum"}).reset_index()
-    total_df.rename(columns={
-        "dteday": "date",
-        "cnt": "total_count"
-    }, inplace=True)
-    return total_df
 
-def create_registered_df(df):
-    registered_df = df.groupby(by="dteday").agg({"registered": "sum"}).reset_index()
-    registered_df.rename(columns={
-        "dteday": "date",
-    }, inplace=True)
-    return registered_df
-
-def create_casual_df(df):
-    casual_df = df.groupby(by="dteday").agg({"casual": "sum"}).reset_index()
-    casual_df.rename(columns={
-        "dteday": "date",
-    }, inplace=True)
-    return casual_df
-
-
-days_data = pd.read_csv("dashboard/alldata.csv", delimiter=",")
-days_data["dteday"] = pd.to_datetime(days_data["dteday"])
-days_data.sample(n=10)
-
-min_date = days_data["dteday"].min()
-max_date = days_data["dteday"].max()
- 
-with st.sidebar:
-    # Menambahkan logo perusahaan
-    st.image(r"dashboard/calendar_image.png")
-    
-    # Mengambil start_date & end_date dari date_input
-    start_date, end_date = st.date_input(
-        label='Rentang Waktu',
-        min_value=min_date,
-        max_value=max_date,
-        value=[min_date, max_date]
-    )
-    
-    
-main_days = days_data[(days_data["dteday"] >= str(start_date)) & 
-                (days_data["dteday"] <= str(end_date))]
-    
-total_data = create_total_df(main_days)
-registered_data = create_registered_df(main_days)
-casual_data = create_casual_df(main_days)
-    
-st.header('Graph of Bicycle Borrowers :chart:')
-
-fig, ax = plt.subplots(figsize=(16, 8))
-st.subheader('Total Borrowers')
-ax.plot(
-    total_data["date"],
-    total_data["total_count"],
-    marker='o', 
-    linewidth=2,
-    color="#90CAF9"
-)
-ax.tick_params(axis='y', labelsize=20)
-ax.tick_params(axis='x', labelsize=15)
-st.pyplot(fig)
-col1, col2 = st.columns(2)
-with col1:
-    total_max = total_data["total_count"].max()
-    st.metric("Maximum", value=total_max)
-with col2:
-    total_min = total_data["total_count"].min()
-    st.metric("Minimum", value=total_min)
-
-tab1, tab2 = st.tabs(["Registered Borrowers", "Casual Borrowers"])
-with tab1:
-    fig,ax = plt.subplots(figsize=(16,8))
-    st.subheader("Registered Borrowers")
-    ax.plot(
-        registered_data["date"],
-        registered_data["registered"],
-        marker="o",
-        linewidth=2,
-        color="#90CAF9"
-    )
-    ax.tick_params(axis="y", labelsize=20)
-    ax.tick_params(axis="x", labelsize=15)
-    st.pyplot(fig)
-    col1, col2 = st.columns(2)
-    with col1:
-        registered_max = registered_data["registered"].max()
-        st.metric("Maximum", value=registered_max)
-    with col2:
-        registered_min = registered_data["registered"].min()
-        st.metric("Minimum", value=total_min)
-with tab2:
-    fig,ax = plt.subplots(figsize=(16,8))
-    st.subheader("Casual Borrowers")
-    ax.plot(
-        casual_data["date"],
-        casual_data["casual"],
-        marker="o",
-        linewidth=2,
-        color="#90CAF9"
-    )
-    ax.tick_params(axis="y", labelsize=20)
-    ax.tick_params(axis="x", labelsize=15)
-    st.pyplot(fig)
-    col1, col2 = st.columns(2)
-    with col1:
-        casual_max = casual_data["casual"].max()
-        st.metric("Maximum", value=casual_max)
-    with col2:
-        casual_min = casual_data["casual"].min()
-        st.metric("Minimum", value=casual_min)
+days_data = pd.read_csv("dashboard\alldata.csv", delimiter=",")
+days_data["dteday"] = pd.to_datetime(days_data["dteday"])        
         
+def create_minMax_data(df):
+    minMax_df = df.groupby(by="season").agg({
+    "instant": "nunique",
+    "cnt": ["max", "min"]
+    }, inplace=True).reset_index()
+    return minMax_df
+
+def create_borrower_data(df):
+    borrower_df = df.groupby(by="workingday").agg({
+    "instant": "nunique",
+    "registered": "mean",
+    "casual": "mean"
+    }, inplace=True)
+    return borrower_df
+
+minMax_data = create_minMax_data(days_data)
+filtered_minMax_data = minMax_data.loc[minMax_data["season"].isin([1,4])]
+borrower_data = create_borrower_data(days_data)
+
+tab1, tab2 = st.tabs(["Question 1", "Question 2"])
+with tab1:
+    fig_min, ax_min = plt.subplots(figsize=(8, 6))
+    ax_min.bar(["Spring", "Winter"], filtered_minMax_data[("cnt", "min")], color="#FF5733")
+    ax_min.set_xlabel("Season", fontsize=12)
+    ax_min.set_ylabel("Minimum Count", fontsize=12)
+    ax_min.set_title("Minimum Counts per Season", fontsize=14)
+    st.subheader("Minimum Counts per Season")
+    st.pyplot(fig_min)
+    
+    fig_max, ax_max = plt.subplots(figsize=(8, 6))
+    ax_min.bar(["Spring", "Winter"], filtered_minMax_data[("cnt", "max")], color="#FF5733")
+    ax_min.set_xlabel("Season", fontsize=12)
+    ax_min.set_ylabel("Maximum Count", fontsize=12)
+    ax_min.set_title("Maximum Counts per Season", fontsize=14)
+    st.subheader("Maximum Counts per Season")
+    st.pyplot(fig_min) 
+with tab2:
+    fig_min, ax_min = plt.subplots(figsize=(8, 6))
+    ax_min.bar(["Spring", "Winter"], borrower_data["registered"], color="#FF5733")
+    ax_min.set_xlabel("Season", fontsize=12)
+    ax_min.set_ylabel("Minimum Count", fontsize=12)
+    ax_min.set_title("Minimum Counts per Season", fontsize=14)
+
+    st.subheader("Minimum Counts per Season")
+    st.pyplot(fig_min)
